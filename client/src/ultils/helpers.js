65f76e45bb387c113ea70a3851919dbc26886f89
Vũ Hải Nam <vuhainam272@gmail.com>
2023-10-16 16:09:11 +0700

    Revert "Remove acckey"
    This reverts commit 3bc52fdd8bddc4551b3463a6c1994f134bab923b.

--- client/src/ultils/helpers.js
+++ client/src/ultils/helpers.js
@@ -1,8 +1,6 @@
 import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 import { auth } from "../config/firebase.config";
 import { v4 as uuidv4 } from "uuid";
-import { CgProfile } from "react-icons/cg";
-import { RiFeedbackLine } from "react-icons/ri";
 
 const googleProider = new GoogleAuthProvider();
 
@@ -13,18 +11,8 @@ export const signInWithGoogle = async () => {
 };
 
 export const Menus = [
-  {
-    id: uuidv4(),
-    icon: <RiFeedbackLine />,
-    name: "Feedbacks",
-    uri: "/feedback",
-  },
-  {
-    id: uuidv4(),
-    icon: <CgProfile />,
-    name: "Settings",
-    uri: "/setting/profile",
-  },
+  { id: uuidv4(), name: "My Feedbacks", uri: "/feedback" },
+  { id: uuidv4(), name: "Settings", uri: "/setting/profile" },
 ];
 
 export const signOutAction = async () => {
