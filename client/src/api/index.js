e1bf52f25899d09f8770250bb0b2b0b6acf1d11b
Vũ Hải Nam <vuhainam272@gmail.com>
2023-10-23 14:21:13 +0700

    Update api global

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const baseURL =
-    "http://127.0.0.1:5001/get-feedback-a0119/us-central1/app";
-// "https://us-central1-get-feedback-a0119.cloudfunctions.net/app";
+    // "http://127.0.0.1:5001/get-feedback-a0119/us-central1/app";
+    "https://us-central1-get-feedback-a0119.cloudfunctions.net/app";
 
 //User
 export const validateUserJWTToken = async (token) => {
