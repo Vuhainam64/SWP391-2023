518e34addb89ba69365196271487bfa301971b5a
Nguyen Anh Duy <aduy1912@gmail.com>
2023-10-28 00:58:55 +0700

    Layout Tasks v1.0 (Fixed from Zwee)

--- client/src/components/Employee/Pendings.jsx
+++
@@ -1,5 +0,0 @@
-function Pendings() {
-  return <div>Pendings</div>;
-}
-
-export default Pendings;
--- client/src/components/Employee/Sidebar.jsx
+++ client/src/components/Employee/Sidebar.jsx
@@ -38,7 +38,7 @@ function Sidebar() {
         </NavLink>
         {/* Pending  */}
         <NavLink
-          to={"/employee/pendings"}
+          to={"/employee/tasks"}
           className={({ isActive }) =>
             isActive
               ? `${isActiveStyles} px-4 py-2 border-l-8  border-slate-600 hover:bg-slate-200  pl-16 p-2w-full font-semibold flex`
@@ -47,7 +47,7 @@ function Sidebar() {
         >
           <div className="flex items-center">
             <MdPendingActions className="text-xl" />
-            <p className="px-1">Pendings</p>
+            <p className="px-1">Tasks</p>
           </div>
         </NavLink>
         {/* complete  */}
