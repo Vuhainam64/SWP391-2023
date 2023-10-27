8c7d859d0f2e26d2d4b01e3d65c0cde6b6a947c9
Nguyen Anh Duy <aduy1912@gmail.com>
2023-10-28 00:33:24 +0700

    Revert "Revert "Layout task v1.0""
    This reverts commit a4c09dd6d5fda3f9a7951d74ca9faa1f434125ee.

--- client/src/components/Employee/index.js
+++ client/src/components/Employee/index.js
@@ -2,5 +2,5 @@ export { default as Sidebar } from "./Sidebar";
 export { default as Main } from "./Main";
 export { default as Home } from "./Home";
 export { default as Feedbacks } from "./Feedbacks";
-export { default as Pendings } from "./Pendings";
+export { default as Pendings } from "./Tasks";
 export { default as Completes } from "./Completes";
